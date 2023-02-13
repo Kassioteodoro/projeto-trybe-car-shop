241d0819b5698046f0deeff6a1754d99652ab467
kassio <kassioteadoro@gmail.com>
2023-02-13 19:23:53 -0300

    começando com TDD

---
+++ tests/unit/Services/Car.test.ts
@@ -0,0 +1,39 @@
+// import { expect } from 'chai';
+// import sinon from 'sinon';
+// import { Model } from 'mongoose';
+
+describe('testando as funcionalidades de Carros', function () {
+  describe('É possivel cadastras um carro', function () {
+    it('com sucesso', async function () {
+      // cenario
+      // const inputValue: ICar = {
+      //   model: 'Marea',
+      //   year: 2002,
+      //   color: 'Black',
+      //   status: true,
+      //   buyValue: 15.990,
+      //   doorsQty: 4,
+      //   seatsQty: 5,
+      // };
+
+      // const returnValue: Car = new Car(
+      //   'Marea',
+      //   2002,
+      //   'Black',
+      //   true,
+      //   15.990,
+      //   4,
+      //   5,
+      // );
+
+      // sinon.stub(Model, 'create').resolves(returnValue);
+
+      // execuçao
+      // const service = new RegisterService();
+      // const result = await service.create(inputValue);
+    
+      // teste
+      // expect(result).to.be.deep.equal(returnValue);
+    });
+  });
+});
